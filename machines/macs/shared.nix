7fd85070956056b896cd5ba675c9b79a9946e1cb
Diego Gutierrez <dgutierrez1287@gmail.com>
2022-07-15 07:38:18 -0400

    testing issue in mac nix config

--- machines/macs/shared.nix
+++ machines/macs/shared.nix
@@ -1,14 +1,13 @@
 {config, pkgs, ...}: {
   # keyboard 
-  system.keyboard.enableKeyMapping = true;
-  system.keyboard.remapCapsLockToEscape = true;
+  # system.keyboard.enableKeyMapping = true;
+  # system.keyboard.remapCapsLockToEscape = true;
 
   # finder changes
   system.defaults.finder.AppleShowAllExtensions = true;
 
   # nix-daemon
   services.nix-daemon.enable = true;
-
 
 
 
