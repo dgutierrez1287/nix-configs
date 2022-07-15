bc400c6dd59da1c12f7e5d17fba4917b06b24b81
Diego Gutierrez <dgutierrez1287@gmail.com>
2022-07-14 22:11:54 -0400

    correcting package issue

--- flake.nix
+++ flake.nix
@@ -10,7 +10,8 @@
 		
 		# url for home manager package
 		home-manager = {
-			url = "github:nix-community/home-manager/release-21.11";
+            #url = "github:nix-community/home-manager/release-21.11";
+            url = "github:nix-community/home-manager";
 			inputs.nixpkgs.follows = "nixpkgs";
 		};
 
