6cbbc864007e06c7a46857c049d22865af291c57
Diego Gutierrez <dgutierrez1287@gmail.com>
2023-08-27 21:36:23 -0400

    added aliases for git

--- users/diego/git.nix
+++ users/diego/git.nix
@@ -7,6 +7,18 @@
 		userEmail = "dgutierrez1287@gmail.com";
 		aliases = {
 			prettylog = "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(r) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative";
+            st = "status";
+            last = "log -1 HEAD --stat";
+            cm = "commit -m";
+            bl = "branches";
+            br = "branch";
+            se = "!git rev-list --all | xargs git grep -F";
+            pb = "push origin";
+            plb = "pull origin";
+            pm = "push origin master";
+            plm = "pull origin master";
+            a = "add";
+            aa = "add -A";
 		};
 		extraConfig = {
 			core.fileMode = true;
