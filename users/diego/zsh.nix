699b19d600fa3779200d866991d444a692e55aad
Diego Gutierrez <dgutierrez1287@gmail.com>
2023-12-24 08:52:03 -0500

    added nixshell zsh plugin

--- users/diego/zsh.nix
+++ users/diego/zsh.nix
@@ -18,6 +18,16 @@
 				src = ./conf/zsh/p10k.zsh;
 				file = "p10k.zsh";
 			}
+            {
+                name = "zsh-nix-shell";
+                file = "nix-shell.plugin.zsh";
+                src = pkgs.fetchFromGitHub {
+                owner = "chisui";
+                repo = "zsh-nix-shell";
+                rev = "v0.7.0";
+                sha256 = "149zh2rm59blr2q458a5irkfh82y3dwdich60s9670kl3cl5h2m1";
+              };
+            }
 		];
 
 		oh-my-zsh = {
